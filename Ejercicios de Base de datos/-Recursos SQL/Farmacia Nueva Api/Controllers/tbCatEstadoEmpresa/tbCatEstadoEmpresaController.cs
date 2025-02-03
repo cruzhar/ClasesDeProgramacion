4e77c24f7b00e7d7be9515c8a35c02e6f753dec4
JosueVargas21 <amaldonadov1@miumg.edu.gt>
2025-02-02 23:21:10 -0600

    medio avanzes

---
+++ Ejercicios de Base de datos/-Recursos SQL/Farmacia Nueva Api/Controllers/tbCatEstadoEmpresa/tbCatEstadoEmpresaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using static Farmacia_Nueva_Api.Modelos.tbCatEstadoEmpresa.tbCatEstadoEmpresa;
+
+namespace Farmacia_Nueva_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class tbCatEstadoEmpresaController : ControllerBase
+    {
+        public readonly string con;
+
+        public tbCatEstadoEmpresaController(IConfiguration configuration)
+        {
+            con = configuration.GetConnectionString("conexion");
+        }
+
+        [HttpGet]
+        public IEnumerable<listarCatEstadoEmpresa> obtenerEstadoEmpresa()
+        {
+            List<listarCatEstadoEmpresa> tbEstadoEmpresa = new List<listarCatEstadoEmpresa>();
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SP_TB_CAT_ESTADO_EMPRESA_LR", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listarCatEstadoEmpresa empresa = new listarCatEstadoEmpresa
+                            {
+                                // Asigna aquí los valores del reader a las propiedades de tu modelo
+                                // Por ejemplo:
+                                // Propiedad1 = reader["NombreColumna"].ToString(),
+                                // Propiedad2 = (int)reader["OtraColumna"],
+                            };
+
+                            tbEstadoEmpresa.Add(empresa);
+                        }
+                    }
+                }
+            }
+
+            return tbEstadoEmpresa;
+        }
+    }
+}
---
+++ Ejercicios de Base de datos/-Recursos SQL/Farmacia Nueva Api/Modelos/tbCatEstadoEmpresa.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Farmacia_Nueva_Api.Modelos.tbCatEstadoEmpresa
+{
+
+    public class tbCatEstadoEmpresa 
+    {
+
+        public int idEstadoEmpresa { get; set; }
+        public int nombreestado { get; set; }
+        public int descripiconEstado { get; set; }
+        public int usuarioCreacion { get; set; }
+        public int fechaCreacion { get; set; }
+        public int usuairoModificacion { get; set; }
+        public int fechaModificacion  { get; set; }
+        public int observaciones { get; set; }
+
+        public class listarCatEstadoEmpresa
+        {
+            public int idEstadoEmpresa { get; set; }
+            public int nombreestado { get; set; }
+            public int descripiconEstado { get; set; }
+            public int usuarioCreacion { get; set; }
+            public int fechaCreacion { get; set; }
+            public int usuairoModificacion { get; set; }
+            public int fechaModificacion { get; set; }
+            public int observaciones { get; set; }
+
+        }
+
+
+
+    }
+
+    
+}
