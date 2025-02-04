fcc657b014db44c6a8fd14069f480d0600cbb623
JosueVargas21 <amaldonadov1@miumg.edu.gt>
2025-02-04 00:29:47 -0600

    m2 V2

---
+++ Ejercicios de Base de datos/-Recursos SQL/Mi_Primer_Api/Clases/ModificarCliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Mi_Primer_Api.Clases
+{
+    public class ModificarCliente
+    {
+        public int CODIGO_CLIENTE { get; set; }
+        public string NOMBRE_CLIENTE { get; set; }
+        public string APELLIDO_CLIENTE { get; set; }
+        public string TELEFONO_CLIENTE { get; set; }
+        public string CORREO_CLIENTE { get; set; }
+
+    }
+}
---
+++ Ejercicios de Base de datos/-Recursos SQL/Mi_Primer_Api/Repositorios/FuncionesGenerales.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Mi_Primer_Api.Repositorios
+{
+    public class FuncionesGenerales
+    {
+        private readonly IConfiguration _configuration;
+        public FuncionesGenerales()
+        {
+         var builder = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory()) .Add.JsonFile() .Add.JsonFile("appsetting.json", optional: false, reloadOnChange: true);
+         _configuration = builder.Build();
+        }
+
+        public DataTable CargarDatos(string sp, Dictionary<string, string>? parametros = null)
+        {
+            using SqlConnection con = new(_configuration.GetConnectionString("ConexionDB").ToString());
+            con.Open();
+            SqlCommand cmd = new(sp, con)
+
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+         if (parametros != null)
+         {
+            
+            foreach (KeyValuePair<string , string> item in parametros)
+            {
+            cmd.Parameters.AddWithValue(Convert.ToString(item.key), Convert.ToString toString(item.Value));
+            }
+
+
+        }
+DataTable dt = new DataTable(); 
+
+    }
+}
