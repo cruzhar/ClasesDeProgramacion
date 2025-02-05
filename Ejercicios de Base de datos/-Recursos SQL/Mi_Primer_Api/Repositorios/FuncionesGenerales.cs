80e23a3d1bef9153fa803310a233be4bb2e2adc2
JosueVargas21 <amaldonadov1@miumg.edu.gt>
2025-02-04 23:01:16 -0600

    MiapiV2

--- Ejercicios de Base de datos/-Recursos SQL/Mi_Primer_Api/Repositorios/FuncionesGenerales.cs
+++ Ejercicios de Base de datos/-Recursos SQL/Mi_Primer_Api/Repositorios/FuncionesGenerales.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Linq.Expressions;
 using Microsoft.Extensions.Configuration;
 
 namespace Mi_Primer_Api.Repositorios
@@ -10,13 +11,14 @@ namespace Mi_Primer_Api.Repositorios
         public FuncionesGenerales()
         {
          var builder = new ConfigurationBuilder()
-          .SetBasePath(Directory.GetCurrentDirectory()) .Add.JsonFile() .Add.JsonFile("appsetting.json", optional: false, reloadOnChange: true);
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile ("appsetting.json", optional: false, reloadOnChange: true);
          _configuration = builder.Build();
         }
 
         public DataTable CargarDatos(string sp, Dictionary<string, string>? parametros = null)
         {
-            using SqlConnection con = new(_configuration.GetConnectionString("ConexionDB").ToString());
+            using SqlConnection con = new(_configuration.GetConnectionString("Conexion").ToString());
             con.Open();
             SqlCommand cmd = new(sp, con)
 
@@ -24,17 +26,28 @@ namespace Mi_Primer_Api.Repositorios
                 CommandType = CommandType.StoredProcedure
             };
 
-         if (parametros != null)
-         {
-            
-            foreach (KeyValuePair<string , string> item in parametros)
+            if (parametros != null)
             {
-            cmd.Parameters.AddWithValue(Convert.ToString(item.key), Convert.ToString toString(item.Value));
+            
+                foreach (KeyValuePair<string , string> item in parametros)
+                {
+                cmd.Parameters.AddWithValue(Convert.ToString(item.key), Convert.ToString toString(item.Value));
+                 }
+
             }
 
-
+            DataTable dt = new DataTable();
+            try
+            {
+                new SqlDataAdapter(cmd).Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            con.Close();  
+            return dt;  
         }
-DataTable dt = new DataTable(); 
-
+            
     }
 }
